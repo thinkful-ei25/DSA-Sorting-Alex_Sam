1a1ea0bd9dd22505254c3a89bee238ce3c8a4e1f
talexcrowell <talexcrowell@gmail.com>
2018-12-20 11:18:39 -0800

    bucket sort finished

--- drills.js
+++ drills.js
@@ -68,20 +68,94 @@ function merge(l, r, arr){
   return arr;
 }
 
+/*
+  input: array , low, high
+
+  [1,10,9,2,8,3,7,4,6,5]
+
+
+  lowest int: 1 
+  highest int: 10
+
+  output: sorted array [1,2,3,4,5,6,7,8,9,10]
+  runtime of O(n)
+
+  Since we know the highest and lowest integer of the data set,
+  create amount of buckets for highest integer (10 = 10 buckets)
+  iterate through original array and place values within associated bucket
+  concatenate the final buckets into  one array
+  return array
+
+*/
+
+function bucketSort(arr, min, max){
+  let buckets =[];
+  let output=[];
+
+  for(let i = min; i<=max; i++){
+    let bucket =[];
+    bucket.push(i);
+    buckets.push(bucket);
+  }
+
+  let i =0;
+  let j= 0;
+  while(i<arr.length-1){
+    if(arr[i] > buckets[j][0]){
+      j++;
+    }
+    if(arr[i] === buckets[j][0]){
+      buckets[j].push(arr[i]);
+      i++;
+    }
+    if(arr[i] < buckets[j][0]){
+      j--;
+    }
+  }
+  buckets.map(item => item.shift());
+
+  for(let i=0; i<buckets.length; i++){
+    if(buckets[i]){
+      output = [...output, ...buckets[i]];
+    }
+  }
+
+  console.log(output);
+}
+
+
+function bSort(arr, min, max){
+  let map = new Map();
+  let output=[];
+  for(let i=min; i<=max;i++){
+    map.set(i, []);
+  }
+  for(let i=0; i<arr.length; i++){
+    let temp = map.get(arr[i]);
+    map.set(arr[i], [...temp, arr[i]]);
+  }
+  for(let i=min; i<=max; i++){
+    output = [...output, ...map.get(i)];
+  }
+  return output;
+}
+
+
 function main(){
   const str = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
   let arr = str.split(' ');
   arr = arr.map(item=>parseInt(item,10));
-  console.log(arr.length);
+  // console.log(arr.length);
   //console.log(arr);
 
   //const answer = qSort(arr);
-  const answer = mSort(arr);
+  // const answer = mSort(arr);
   
-  console.log(answer);
-  console.log(countM);
+  // console.log(answer);
+  // console.log(countM);
   //console.log(countQ);
 
+  bucketSort([1,10,9,6,8,8,7,4,5,6,5], 1, 10);
 }
 
 main();
